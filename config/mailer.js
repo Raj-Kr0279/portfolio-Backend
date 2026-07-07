326a9869be515cfef93b2a282409f3c6d2f97fa8
Raj-Kr0279 <kumarrajendra8802@gmail.com>
2026-07-07 12:27:45 +0530

    smtp add

--- config/mailer.js
+++ config/mailer.js
@@ -1,6 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMP_PORT,           // 465 or 587
+  secure: process.env.SMTP_HOST === 'smtp.gmail.com' ? true : false,
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
