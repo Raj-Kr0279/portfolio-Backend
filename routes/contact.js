1cb61c88e845370997b3fd7a4709c2ff0dfadfb4
Raj-Kr0279 <kumarrajendra8802@gmail.com>
2026-07-07 10:57:25 +0530

    first commit

---
+++ routes/contact.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const Contact = require("../models/Contact");
+
+router.post("/", async (req, res) => {
+  try {
+    const contact = new Contact(req.body);
+
+    await contact.save();
+
+    res.json({
+      success: true,
+      message: "Message received!",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+module.exports = router;
