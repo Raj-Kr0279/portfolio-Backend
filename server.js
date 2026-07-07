c52fad1ad3fc4fd4bd4d2685e549d9560ff2a44c
Raj-Kr0279 <kumarrajendra8802@gmail.com>
2026-07-07 11:05:35 +0530

    dynamic port

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ mongoose.connect(process.env.MONGO_URI);
 
 app.use("/api/contact", require("./routes/contact"));
 
-app.listen(5000, () => {
-  console.log("Server running")
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
